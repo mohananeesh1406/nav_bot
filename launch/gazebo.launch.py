ee5e9405f75ed0082b27c345c309729401bf1f73
Mohan Aneesh <mohannitt14@gmail.com>
2026-08-12 06:42:02 +0530

    Updated launch file and works with teleop with twist

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -1,41 +1,64 @@
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():
 
-    package_name = 'my_robot_description'
+    # ---------------------------------------------------------
+    # Package paths
+    # ---------------------------------------------------------
 
-    pkg_share = get_package_share_directory(package_name)
+    pkg_my_robot_description = get_package_share_directory(
+        'my_robot_description'
+    )
+
+    pkg_ros_gz_sim = get_package_share_directory(
+        'ros_gz_sim'
+    )
 
     urdf_file = os.path.join(
-        pkg_share,
+        pkg_my_robot_description,
         'urdf',
         'robot.urdf'
     )
 
+    # ---------------------------------------------------------
     # Read URDF
-    with open(urdf_file, 'r') as file:
-        robot_description = file.read()
+    # ---------------------------------------------------------
 
-    # Start Ignition Gazebo 6
-    gazebo = ExecuteProcess(
-        cmd=[
-            'ign',
-            'gazebo',
-            '-r',
-            'empty.sdf'
-        ],
-        output='screen'
+    with open(urdf_file, 'r') as f:
+        robot_description = f.read()
+
+    # ---------------------------------------------------------
+    # Gazebo Fortress
+    # ---------------------------------------------------------
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                pkg_ros_gz_sim,
+                'launch',
+                'gz_sim.launch.py'
+            )
+        ),
+        launch_arguments={
+            'gz_args': '-r empty.sdf'
+        }.items()
     )
 
+    # ---------------------------------------------------------
     # Robot State Publisher
+    # ---------------------------------------------------------
+
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -48,45 +71,91 @@ def generate_launch_description():
         ]
     )
 
-    joint_state_publisher_gui = Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui'
-        )
+    ros_gz_bridge = Node(
+    package='ros_gz_bridge',
+    executable='parameter_bridge',
+    arguments=[
+        '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'
+    ],
+    output='screen',
+    )
 
-    
+    # ---------------------------------------------------------
+    # Spawn robot into Gazebo
+    # ---------------------------------------------------------
 
-    # Spawn URDF into Ignition Gazebo
-    spawn_robot = ExecuteProcess(
-        cmd=[
-            'ign',
-            'service',
-            '-s',
-            '/world/empty/create',
-            '--reqtype',
-            'ignition.msgs.EntityFactory',
-            '--reptype',
-            'ignition.msgs.Boolean',
-            '--timeout',
-            '5000',
-            '--req',
-            'sdf_filename: "' + urdf_file + '", '
-            'name: "my_robot"'
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'my_robot',
+            '-file', urdf_file,
+            '-x', '0.0',
+            '-y', '0.0',
+            '-z', '0.1',
         ],
         output='screen'
     )
 
-    rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen'
+    # ---------------------------------------------------------
+    # joint state broadcaster
+    # ---------------------------------------------------------
+
+    joint_state_broadcaster_spawner = Node(
+    package='controller_manager',
+    executable='spawner',
+    arguments=[
+        'joint_state_broadcaster',
+        '--controller-manager',
+        '/controller_manager',
+    ],
+    output='screen',
     )
 
+    # ---------------------------------------------------------
+    # diff drive controller
+    # ---------------------------------------------------------
+
+    diff_drive_controller_spawner = Node(
+    package='controller_manager',
+    executable='spawner',
+    arguments=[
+        'diff_drive_controller',
+        '--controller-manager',
+        '/controller_manager',
+    ],
+    output='screen',
+    )
+
+
+    start_joint_state_broadcaster = RegisterEventHandler(
+        OnProcessExit(
+            target_action=spawn_robot,
+            on_exit=[
+                joint_state_broadcaster_spawner
+            ]
+        )
+    )
+
+    start_diff_drive_controller = RegisterEventHandler(
+        OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[
+                diff_drive_controller_spawner
+            ]
+        )
+    )
+
+
+    # ---------------------------------------------------------
+    # Launch everything
+    # ---------------------------------------------------------
+
     return LaunchDescription([
-        gazebo,
-        robot_state_publisher,
-        joint_state_publisher_gui,
-        spawn_robot,
-        rviz
+    gazebo,
+    ros_gz_bridge,
+    robot_state_publisher,
+    spawn_robot,
+    start_joint_state_broadcaster,
+    start_diff_drive_controller,
     ])
